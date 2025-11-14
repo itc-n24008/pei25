36637ab2d82bab8818eaa33cd084e323c09243d0
itc-n24008 <n24008@std.it-college.ac.jp>
2025-11-14 01:27:35 +0000

    kadai

--- m2/m2_p4_19.py
+++ m2/m2_p4_19.py
@@ -1,4 +1,4 @@
 a = 6
-b = pow(a, 3)
-c = divmod(d, a)
+b = pow(a, 3) # aの３乗、つまり216となる
+c = divmod(b, a) # bをaでワット時の商とあまりをダブルで
 print(c)
---
+++ m2/m2_p4_19kadai.py
@@ -0,0 +1,4 @@
+x = 5
+a = pow(x, 2)
+b = pow(x, 3)
+print(a, b)
